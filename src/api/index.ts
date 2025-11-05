1039e2507ea791dd9695c508867f73da3b2bf20b
maxmalato <max.malato@hotmail.com>
2025-11-05 12:51:28 -0300

    feat: atualizar interfaces de CNPJ e Inscrição Estadual, e adicionar suporte a geração de senhas

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,28 +1,60 @@
 import axios from "axios";
-import { CnpjData } from "@/types";
+import {
+  CnpjDataResponse,
+  GenerateInscricaoEstadualdRequest,
+  GeneratePasswordRequest,
+  GeneratePasswordResponse,
+} from "@/types";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-// Função para buscar dados do CNPJ
-export const consultarCnpj = async (cnpj: string): Promise<CnpjData> => {
-  // Limpar o CNPJ para deixar apenas números
+if (!import.meta.env.VITE_API_BASE_URL) {
+  console.error(
+    "VITE_API_BASE_URL não está definida no .env.local. A API não funcionará."
+  );
+}
+
+// FUNÇÕES DA API //
+
+//* Consultar dados de um CNPJ
+export const consultarCnpj = async (
+  cnpj: string
+): Promise<CnpjDataResponse> => {
+  // Limpar CNPJ para deixar apenas números
   const cnpjNumeros = cnpj.replace(/\D/g, "");
-  const { data } = await apiClient.get<CnpjData>(`/cnpj/${cnpjNumeros}`);
+
+  const { data } = await apiClient.get<CnpjDataResponse>(
+    `/cnpj/${cnpjNumeros}`
+  );
 
   return data;
 };
 
-// Função para gerar Inscrição Estadual
-interface GerarIEResponse {
-  ie: string;
-}
+//* Gerar dados de um CNPJ
+export const gerarCnpj = async (): Promise<string> => {
+  const { data } = await apiClient.get<string[]>(`/cnpj?new=true`);
 
+  return data[0];
+};
+
+//* Gerar dados de uma Inscrição Estadual
+//! Verificar se irá funcionar
 export const gerarInscricaoEstadual = async (
-  uf: string
-): Promise<GerarIEResponse> => {
-  const { data } = await apiClient.post(`/gerar-ie`, { estado: uf });
+  uf: GenerateInscricaoEstadualdRequest["uf"]
+): Promise<string> => {
+  const { data } = await apiClient.get<string[]>(`/inscricao-estadual/random?${uf}`)
+
+  return data[0];
+};
+
+//* Gerar senha
+//! Verificar se irá funcionar
+export const gerarSenha = async (
+  payload: GeneratePasswordRequest
+): Promise<GeneratePasswordResponse> => {
+  const { data } = await apiClient.post<GeneratePasswordResponse>(`/password/generate?${payload}`)
 
   return data;
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,44 @@
-// Interface do CNPJ
-export interface CnpjData {
+// Consultar CNPJ
+export interface CnpjDataRequest {
   cnpj: string;
-  razaoSocial: string;
-  nomeFantasia: string;
-  situacao: string;
+}
+export interface CnpjDataResponse {
+  razao_social: string;
+  nome_fantasia: string;
+  cnpj: string;
   logradouro: string;
   numero: string;
   bairro: string;
   municipio: string;
   uf: string;
   cep: string;
+  descricao_situacao_cadastral: string;
+  cnae_fiscal_descricao: string;
 }
 
-// Interface do Gerar senha
-export interface PasswordData {
+// Gerar CNPJ
+export interface GenerateCnpj {
+  formatted: string;
+  unformatted: string;
+}
+
+// Gerar senhas
+export interface GeneratePasswordRequest {
+  length?: number; // mínimo 4, máximo 128, padrão 12
+  letters?: boolean; // padrão true
+  numbers?: boolean; // padrão true
+  symbols?: boolean; // padrão false
+}
+export interface GeneratePasswordResponse {
   password: string;
+}
+
+// Gerar Inscrição Estadual
+export interface GenerateInscricaoEstadualdRequest {
+  uf: string;
+}
+
+export interface GenerateInscricaoEstadualdResponse {
+  uf: string;
+  inscricaoEstadual: string;
 }
