9c3cc08afac9c91ce1c8cd83c3f4f2ce7e09e718
maxmalato <max.malato@hotmail.com>
2025-11-07 14:28:46 -0300

    feat: adicionar suporte a temas com o componente Toaster e integrar na MainLayout; atualizar dependências no package.json e package-lock.json

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../shared/Header";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function MainLayout() {
     return (
         <main>
             <Header/>
             <Outlet />
+            <Toaster richColors/>
         </main>
     )
 };
--- src/pages/GerarSenha.tsx
+++ src/pages/GerarSenha.tsx
@@ -1,48 +1,92 @@
+import { useState } from "react";
 import { Copy, RotateCcw } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+
+// Importar função da API e os tipos
+import { gerarSenha } from "@/api";
+import type { GeneratePasswordRequest } from "@/types";
+
+// Importar componentes
 import { Slider } from "@/components/ui/slider";
 import { CheckboxShared } from "@/components/shared/CheckboxShared";
 import { ButtonShared } from "@/components/shared/ButtonShared";
-import { useState } from "react";
+import { toast } from "sonner";
 
 
 export function GerarSenha() {
-    const [includeUppercase, setIncludeUppercase] = useState(true);
-    const [includeLowercase, setIncludeLowercase] = useState(true);
+    const [passwordLength, setPasswordLength] = useState(12);
+    const [includeLetters, setIncludeLetters] = useState(true);
     const [includeNumbers, setIncludeNumbers] = useState(true);
     const [includeSymbols, setIncludeSymbols] = useState(true);
+
+    // Lógica da API "gerarSenha"
+    const { mutate, data, isPending, error } = useMutation({
+        mutationFn: gerarSenha,
+        onError: (err) => {
+            toast.error("Erro ao gerar a senha.", {
+                description: err.message || "Não possível conectar a API.",
+            });
+        },
+    });
+
+    const generatedPassword = data?.password || "";
+
+    const handleGeneratePassword = () => {
+        const options: GeneratePasswordRequest = {
+            length: passwordLength,
+            letters: includeLetters,
+            numbers: includeNumbers,
+            symbols: includeSymbols,
+        };
+
+        mutate(options);
+    };
+
+    const handleCopy = () => {
+        if (!generatedPassword) return;
+
+        navigator.clipboard.writeText(generatedPassword);
+
+        toast.success("Copiado!", {
+            description: "Senha copiada para a área de transferência."
+        });
+    };
 
     return (
         <main className="mt-10">
             <h1 className="font-bold text-2xl text-center mb-4">Gerador de senhas</h1>
 
             <section className="flex gap-4 justify-between items-center bg-[#f1f5f9] rounded-lg p-4">
-                <p className="font-bold text-[#26a8ed]">HJA#@!JJJAKSJK@</p>
-                <div className="bg-[#c5e5f6] p-2 rounded-lg cursor-pointer">
+                <p className={`font-bold text-lg flex-grow ${generatedPassword ? "text-[#26a8ed]" : "text-gray-400"}`}>{generatedPassword || "Sua senha aparecerá aqui"}</p>
+                <div
+                    onClick={handleCopy}
+                    className="bg-[#c5e5f6] p-2 rounded-lg cursor-pointer transition-transform active:scale-90">
                     <Copy color="#26a8ed" />
                 </div>
             </section>
 
-            <section>
+            <section className="mt-6">
                 <div className="flex justify-between mb-4">
                     <p className="font-semibold">Tamanho da senha</p>
-                    <span className="font-bold text-xl text-[#26a8ed]">12</span>
+                    <span className="font-bold text-xl text-[#26a8ed]">{passwordLength}</span>
                 </div>
-                <Slider defaultValue={[33]} max={100} step={1} className="bg-gray-300" />
+                <Slider
+                    value={[passwordLength]}
+                    onValueChange={(value) => setPasswordLength(value[0])}
+                    min={4}
+                    max={128}
+                    step={1}
+                    className="bg-gray-300"
+                    disabled={isPending}
+                />
             </section>
 
-            <section className="space-y-4 mt-6 md:grid md:grid-cols-2 md:space-y-0 md:gap-4">
+            <section className="space-y-4 mt-6 md:grid md:grid-cols-2 md:space-y-0 md:gap-4 lg:grid-cols-3">
                 <CheckboxShared
-                    id="uppercase"
-                    label="Maiúsculas"
-                    checked={includeUppercase}
-                    onCheckedChange={setIncludeUppercase}
-                />
-
-                <CheckboxShared
-                    id="lowercase"
-                    label="Minúsculas"
-                    checked={includeLowercase}
-                    onCheckedChange={setIncludeLowercase}
+                    id="letters"
+                    label="Letras"
+                    checked={includeLetters}
+                    onCheckedChange={setIncludeLetters}
                 />
                 <CheckboxShared
                     id="numbers"
@@ -58,7 +102,16 @@ export function GerarSenha() {
                 />
             </section>
 
-            <ButtonShared title="Gerar senha" Icon={RotateCcw} />
+            <ButtonShared
+                title={isPending ? "Gerando..." : "Gerar senha"}
+                Icon={RotateCcw}
+                onClick={handleGeneratePassword}
+                disabled={isPending}
+            />
+
+            {error && (
+                <p className="text-red-500 text-center mt-4">{error.message}</p>
+            )}
         </main>
-    )
+    );
 }
